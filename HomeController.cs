a22378fad89d3381484398a6dc9aac6b0c3cce4a
vicktor500 <161779451+vicktor500@users.noreply.github.com>
2025-03-17 21:28:55 -0400

    Add files via upload

--- HomeController.cs
+++ HomeController.cs
@@ -1,6 +1,9 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ElectronicSafari.Models;
+using Npgsql;
+using System.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace ElectronicSafari.Controllers;
 
@@ -8,13 +11,36 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly DatabaseConnection _dbConnection;
+
+    public HomeController(ILogger<HomeController> logger, DatabaseConnection databaseConnection)
     {
         _logger = logger;
+        _dbConnection = databaseConnection;
     }
 
     public IActionResult Index()
     {
+        
+ using var conn = _dbConnection.GetConnection();
+        using var cmd = new NpgsqlCommand("SELECT a.nome, a.email from usuario a", conn);
+        using var reader = cmd.ExecuteReader();
+
+        int cont = 0;
+        var nomes = new List<string>();
+        var emails = new List<string>();
+
+        while(reader.Read()){
+
+            nomes.Add(reader.GetString(0));
+            emails.Add(reader.GetString(1));
+            cont++;
+        
+        }
+
+        ViewData["nomes"] = nomes;
+        ViewData["emails"] = emails;
+        ViewData["cont"] = cont;
         return View();
     }
 
--- UserRepository.cs
+++ UserRepository.cs
@@ -1,24 +1,49 @@
-using System.Data.Common;
-using ElectronicSafari;
+
+using Microsoft.AspNetCore.Mvc;
+using ElectronicSafari.Models;
 using Npgsql;
+using System.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
 
 namespace ElectronicSafari.Repository;
 
 public class UserRepository{
     private readonly DatabaseConnection _dbConnection;
+    private readonly IWebHostEnvironment _webHostEnvironmen;
 
-    public UserRepository(DatabaseConnection databaseConnection){
+    public UserRepository(DatabaseConnection databaseConnection,IWebHostEnvironment webHostEnvironment){
 
         _dbConnection = databaseConnection;
+        _webHostEnvironmen = webHostEnvironment;
     }
 
-    public List<User> ListUser(){
-        
-        var user = new List<User>();
+    public List<User> ListUser()
+    {
+      
+        var users = new List<User>();
 
-        using(var connection = _dbConnection.GetConnection()){
+        using (var connection = _dbConnection.GetConnection()){
             
-            using (var command = new NpgsqlCommand("Select id_usuario,nome,login,email from usuario"))
+            using (var command = new NpgsqlCommand("Select id_usuario,nome,login,email from usuario",(NpgsqlConnection)connection))
+            {
+                using (var reader = command.ExecuteReader()){
+
+                    while(reader.Read())
+                    {
+                        users.Add(new User
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Login = reader.GetString(2),
+                            Email = reader.GetString(3),
+                            Photo = reader.IsDBNull(4)? null: reader.GetString(4),
+                        });
+                    }
+                }
+            }
         }
+        return users;
     }
+
 }
